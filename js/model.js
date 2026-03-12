d99e7499b09dc5bcfdc9a8c65093f6d0cd2e36d6
IvanMagalhaesDev <ivan.magalhaes06@aluno.ifce.edu.br>
2026-03-12 15:37:39 -0300

    fix: organização do array de imagens

--- js/model.js
+++ js/model.js
@@ -4,37 +4,31 @@ class GalleryModel {
        ............................................................ */
     constructor() {
         // Nosso "Banco de Dados" fictício. Um array de objetos com todas as informações das fotos.
-        this.images = [
-    // CIDADE
+    this.images = [
     { id: 1, src: 'img/cidade1.jpg', title: 'Skyline Urbano', description: 'Vista urbana em Madalena', category: 'cidade' },
-    { id: 2, src: 'img/cidade2.jpg', title: 'Ruas Históricas', description: 'Visitando o interior do meu pai', category: 'cidade' },
-    { id: 3, src: 'img/cidade3.jpg', title: 'Vida Noturna', description: 'Terra da minha amiga da escola: Jéssica', category: 'cidade' },
-    { id: 4, src: 'img/cidade4.jpg', title: 'Ponte Iluminada', description: 'Valha-me São Francisco #Canindé', category: 'cidade' },
+    { id: 2, src: 'img/tecnologia1.jpg', title: 'Futurista', description: 'Esse perafone é mesmo um sucesso', category: 'tecnologia' },
+    { id: 3, src: 'img/pessoas1.jpg', title: 'Niver', description: 'Aniversário do priminho', category: 'pessoas' },
+    { id: 4, src: 'img/famosos1.jpg', title: 'Car Ride', description: 'Lindsay Lohan, Britney Spears e Paris Hilton', category: 'famosos' },
+    { id: 5, src: 'img/pelucia1.jpg', title: 'Urso de Pelúcia', description: 'Meu urso favorito', category: 'pelucia' },
 
-    // TECNOLOGIA
-    { id: 5, src: 'img/tecnologia1.jpg', title: 'Futurista', description: 'Esse perafone é mesmo um sucesso', category: 'tecnologia' },
-    { id: 6, src: 'img/tecnologia2.jpg', title: 'Netbook novo', description: 'Como usar esse mini computador, aka netbook?', category: 'tecnologia' },
-    { id: 7, src: 'img/tecnologia3.jpg', title: 'Câmera Digital', description: 'Valeu pelo presente, pai! #cybershot', category: 'tecnologia' },
-    { id: 8, src: 'img/tecnologia4.jpg', title: 'PS2', description: 'No ponto de jogar Amor Doce nesse #PS2', category: 'tecnologia' },
+    { id: 6, src: 'img/cidade2.jpg', title: 'Ruas Históricas', description: 'Visitando o interior do meu pai', category: 'cidade' },
+    { id: 7, src: 'img/tecnologia2.jpg', title: 'Netbook novo', description: 'Como usar esse mini computador, aka netbook?', category: 'tecnologia' },
+    { id: 8, src: 'img/pessoas2.jpg', title: 'Momento de Alegria', description: 'Patati Patatá', category: 'pessoas' },
+    { id: 9, src: 'img/famosos2.jpg', title: 'High School Musical', description: 'Premiere de HSM ❤️', category: 'famosos' },
+    { id: 10, src: 'img/pelucia2.jpg', title: 'Coelho de Pelúcia', description: 'Presente de aniversário', category: 'pelucia' },
 
-    // PESSOAS
-    { id: 9, src: 'img/pessoas1.jpg', title: 'Niver', description: 'Aniversário do priminho', category: 'pessoas' },
-    { id: 10, src: 'img/pessoas2.jpg', title: 'Momento de Alegria', description: 'Patati Patatá', category: 'pessoas' },
-    { id: 11, src: 'img/pessoas3.jpg', title: 'São João', description: 'Festa Junina da EP', category: 'pessoas' },
-    { id: 12, src: 'img/pessoas4.jpg', title: 'Amigas', description: 'BFFs', category: 'pessoas' },
+    { id: 11, src: 'img/cidade3.jpg', title: 'Vida Noturna', description: 'Terra da minha amiga da escola: Jéssica', category: 'cidade' },
+    { id: 12, src: 'img/tecnologia3.jpg', title: 'Câmera Digital', description: 'Valeu pelo presente, pai! #cybershot', category: 'tecnologia' },
+    { id: 13, src: 'img/pessoas3.jpg', title: 'São João', description: 'Festa Junina da EP', category: 'pessoas' },
+    { id: 14, src: 'img/famosos3.jpg', title: 'Heróis da Terra', description: 'Absolute filme do ano!!! #XMen', category: 'famosos' },
+    { id: 15, src: 'img/pelucia3.jpg', title: 'Panda Fofo', description: 'Muito macio e fofo', category: 'pelucia' },
 
-    // FAMOSOS
-    { id: 13, src: 'img/famosos1.jpg', title: 'Car Ride', description: 'Lindsay Lohan, Britney Spears e Paris Hilton', category: 'famosos' },
-    { id: 14, src: 'img/famosos2.jpg', title: 'High School Musical', description: 'Premiere de HSM ❤️', category: 'famosos' },
-    { id: 15, src: 'img/famosos3.jpg', title: 'Heróis da Terra', description: 'Absolute filme do ano!!! #XMen', category: 'famosos' },
-    { id: 16, src: 'img/famosos4.jpg', title: 'Rebelde', description: 'Y soy rebelde #amoeles', category: 'famosos' },
-
-    // PELÚCIA
-    { id: 17, src: 'img/pelucia1.jpg', title: 'Urso de Pelúcia', description: 'Meu urso favorito', category: 'pelucia' },
-    { id: 18, src: 'img/pelucia2.jpg', title: 'Coelho de Pelúcia', description: 'Presente de aniversário', category: 'pelucia' },
-    { id: 19, src: 'img/pelucia3.jpg', title: 'Panda Fofo', description: 'Muito macio e fofo', category: 'pelucia' },
+    { id: 16, src: 'img/cidade4.jpg', title: 'Ponte Iluminada', description: 'Valha-me São Francisco #Canindé', category: 'cidade' },
+    { id: 17, src: 'img/tecnologia4.jpg', title: 'PS2', description: 'No ponto de jogar Amor Doce nesse #PS2', category: 'tecnologia' },
+    { id: 18, src: 'img/pessoas4.jpg', title: 'Amigas', description: 'BFFs', category: 'pessoas' },
+    { id: 19, src: 'img/famosos4.jpg', title: 'Rebelde', description: 'Y soy rebelde #amoeles', category: 'famosos' },
     { id: 20, src: 'img/pelucia4.jpg', title: 'Tigre de Pelúcia', description: 'Companheiro de aventuras', category: 'pelucia' }
-    ];
+];
 
         // Estado inicial do aplicativo (como ele deve abrir pela primeira vez)
         this.currentCategory = 'all'; // Categoria atual selecionada
